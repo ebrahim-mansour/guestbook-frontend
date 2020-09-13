d227c331874d4ef176f16a8972a2ba9bd6b4287e
ebrahimmansour <ebrahimmansour2001@gmail.com>
2020-09-13 15:29:02 +0200

    initialzing app with needed routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
-function App() {
+import { AuthContext } from "./shared/context/auth-context";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import { useAuth } from "./shared/hooks/auth-hook";
+
+const App = () => {
+  const { token, userId, login, logout } = useAuth();
+  let routes;
+
+  if (token) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <div>All users</div>
+        </Route>
+        <Route path="/:userId/messages" exact>
+          <div>User messages</div>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <div>All users</div>
+        </Route>
+        <Route path="/:userId/messages" exact>
+          <div>User messages</div>
+        </Route>
+        <Route path="/auth" exact>
+          <div>Authenicate</div>
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider
+      value={{ isLoggedIn: !!token, token, userId, login, logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
   );
-}
+};
 
 export default App;
